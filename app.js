74aeef527aeecf03e8aecfabd7094f594d298520
Milani <milaniyoges@gmail.com>
2023-05-07 17:42:13 +0530

    Hashing md5

--- app.js
+++ app.js
@@ -3,11 +3,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5=require("md5");
 
 const app = express();
-
-console.log(process.env.API_KEY);
 
 app.use(express.static("public"));
 app.set("view engine", 'ejs');
@@ -19,8 +17,6 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
-
-userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:['password']});
 
 const User = new mongoose.model("User", userSchema);
 
@@ -40,7 +36,7 @@ app.get("/register", async (req, res) => {
 app.post("/register", async (req, res) => {
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     const user = await newUser.save();
@@ -52,7 +48,7 @@ app.post("/register", async (req, res) => {
 
 app.post("/login", async (req, res) => {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
 
     const foundUser = await User.findOne({ email: username });
 
